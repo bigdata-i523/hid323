c02f9ecae569f0684a6872649c4c79810f91827a
UMAKUGAN <31508874+UMAKUGAN@users.noreply.github.com>
2017-12-04 13:14:50 -0700

    Update setup.py

--- project/setup.py
+++ project/setup.py
@@ -1,1 +1,11 @@
+from distutils.core import setup
+
+setup(name='Dockerswarm',
+      version='1.0',
+      description='Rasberry Pi Docker Swarm Cluster',
+      author='Uma M Kugan',
+      author_email='umakugan@iu.edu',
+      url='https://github.com/bigdata-i523/hid323/edit/master/project/',
+      packages=['dockerswarm'],
+     )
 
