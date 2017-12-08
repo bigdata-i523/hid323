dbbf7eb0cc6424480cfb9d9a120553de763237ed
UMAKUGAN <31508874+UMAKUGAN@users.noreply.github.com>
2017-12-08 11:05:00 -0700

    Update docker.py

--- project/code/docker.py
+++ project/code/docker.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import subprocess
 import sys
 import threading
@@ -13,17 +14,32 @@ def call(cmd, ignore_return_code=False):
    else:
        return stdout
 
-def docker_instal(hostnm):
-	self.call("ssh $hostnm curl -sSL https://get.docker.com | sh”)
 
-    
-def swarm_instal(hostnm,hosttyp):
-	if $hosttyp == “Master”:
-         	self.call("sudo docker swarm init --advertise-addr " + $hostnm)
-       	else:
-          	jointkn = self.call("sudo docker swarm join-token worker”)
-          	testsh = “ssh $hostnm  + self.call(jointkn)”
-		self.call(testsh)
+def docker_instal(hostnm):
+    cmd = 'ssh hostnm curl -sSL https://get.docker.com | sh'
+    self.call(cmd)
+
+
+def swarm_instal(hostnm, hosttyp):
+    if hosttyp == "Master":
+        self.call("sudo docker swarm init --advertise-addr " + hostnm)
+    else:
+        joincmd = 'sudo docker swarm join-token worker'
+        jointkn = self.call(joincmd)
+        testsh = "ssh hostnm + self.call(jointkn)"
+        self.call(testsh)
 
 def pull(self):
-   nodes = self.call("docker node ls | grep Ready | awk -F'[[:space:]][[:space:]]+' '{print $2}'").rstrip().split('\n')
+    nodes = self.call("docker node ls | grep Ready | awk -F'[[:space:]][[:space:]]+' '{print $2}'").rstrip().split('\n')
+
+def instal(self):
+    step1 = self.call("sudo apt-get update -y")
+    step1.wait()
+    step2 = self.call("sudo apt-get -y install curl")
+    step2.wait()
+    step3 = self.call("sudo apt-get upgrade -y")
+    step3.wait()
+    step4 = self.call("sudo apt-get dist-upgrade -y")
+    step4.wait()
+    step5 = self.call("sudo apt-get -y install raspberrypi-ui-mods")
+    step5.wait()
