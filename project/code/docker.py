0c94e2050316ca6925e3c541591054dfc196242e
UMAKUGAN <31508874+UMAKUGAN@users.noreply.github.com>
2017-12-11 17:23:54 -0700

    Update docker.py

--- project/code/docker.py
+++ project/code/docker.py
@@ -4,6 +4,7 @@ import sys
 import threading
 import os
 import argparse
+import yaml
 
 def callproc(cmd, ignore_return_code=False):
    print('Running: \n' + cmd + '\n')
@@ -17,15 +18,22 @@ def callproc(cmd, ignore_return_code=False):
        return stdout
 
 
-def docker_instal(hostnm):
-    cmd = 'ssh ' + hostnm ' curl -sSL https://get.docker.com | sh'
+def docker_instal(hostname):
+    cmd =  'curl -sSL https://get.docker.com | sh'
+    execssh(hostname, cmd)
+    
+
+def execssh(hostname, cmd, username=None):
+    if username is None:
+        cmd = 'ssh ' + hostname + ' "' + cmd + '"'     
+    else:     
+        cmd = 'ssh ' + username + '@' + hostname + ' "' + cmd + '"'
     callproc(cmd)
-
-
-def swarm_instal(hostnm, hosttyp):
+      
+def swarm_instal(hostname, hosttyp):
     if hosttyp == "Master":
-        swrmcmd = 'ssh ' + hostnm + " sudo docker swarm init "
-        callproc(swrmcmd)
+        swarmcmd = 'sudo docker swarm init'
+        execssh(hostname,swarmcmd)
     else:
         joincmd = 'sudo docker swarm join-token worker'
         jointkn = callproc(joincmd)
@@ -36,11 +44,22 @@ def pull():
     nodes = callproc("docker node ls | grep Ready | awk -F'[[:space:]][[:space:]]+' '{print $2}'").rstrip().split('\n')
 
 def instal():
-    step1 = callproc("sudo apt-get update -y")
-    step2 = callproc("sudo apt-get -y install curl")
-    step3 = callproc("sudo apt-get upgrade -y")
-    step4 = callproc("sudo apt-get dist-upgrade -y")
-    step5 = callproc("sudo apt-get -y install raspberrypi-ui-mods")
+   # step1 = callproc("sudo apt-get update -y")
+   # step2 = callproc("sudo apt-get -y install curl")
+   # step3 = callproc("sudo apt-get upgrade -y")
+   # step4 = callproc("sudo apt-get dist-upgrade -y")
+   # step5 = callproc("sudo apt-get -y install raspberrypi-ui-mods")
+      
+    commands='''
+       sudo apt-get update -y
+       sudo apt-get -y install curl
+       sudo apt-get upgrade -y
+       sudo apt-get dist-upgrade -y
+       sudo apt-get -y install raspberrypi-ui-mods
+       '''
+    for line in commands:
+         # print (line)
+        step=callproc(line)
 
 def main(hostnm1, master):
     parser = argparse.ArgumentParser()
@@ -51,6 +70,12 @@ def main(hostnm1, master):
     instal()
     docker_instal(args.hostnm)
     swarm_instal(args.hostnm, args.hosttyp)
+
+f = open('docker-config.yaml')
+config = yaml.load(f)
+f.close()
+print config['master']
+print config['worker']
 
 
 if __name__ == "__main__":
