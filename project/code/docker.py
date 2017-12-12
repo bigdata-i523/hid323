0f2ff6fd8bcf619719fcaa640bd56f55a8bfeb29
UMAKUGAN <31508874+UMAKUGAN@users.noreply.github.com>
2017-12-12 02:30:19 -0700

    Update docker.py

--- project/code/docker.py
+++ project/code/docker.py
@@ -1,9 +1,7 @@
 # coding=utf-8
 import subprocess
 import sys
-import threading
 import os
-import argparse
 import yaml
 
 def callproc(cmd, ignore_return_code=False):
@@ -36,20 +34,14 @@ def swarm_instal(hostname, hosttyp):
         execssh(hostname,swarmcmd)
     else:
         joincmd = 'sudo docker swarm join-token worker'
-        jointkn = callproc(joincmd)
-        testsh = 'ssh ' + hostnm + ' ' + callproc(jointkn)
-        callproc(testsh)
-
+        jointkn = execssh(hostname,joincmd)
+        execssh(jointkn)
+        
 def pull():
     nodes = callproc("docker node ls | grep Ready | awk -F'[[:space:]][[:space:]]+' '{print $2}'").rstrip().split('\n')
 
 def instal():
-   # step1 = callproc("sudo apt-get update -y")
-   # step2 = callproc("sudo apt-get -y install curl")
-   # step3 = callproc("sudo apt-get upgrade -y")
-   # step4 = callproc("sudo apt-get dist-upgrade -y")
-   # step5 = callproc("sudo apt-get -y install raspberrypi-ui-mods")
-      
+   
     commands='''
        sudo apt-get update -y
        sudo apt-get -y install curl
@@ -61,24 +53,18 @@ def instal():
          # print (line)
         step=callproc(line)
 
- # def main(hostnm1, master):
- #   parser = argparse.ArgumentParser()
- #   parser.add_argument("hostnm", help="Enter the Host Name")
- #   parser.add_argument("hosttyp", help="Please Enter Manager or Worker")
- #   args = parser.parse_args()
-
- 
+  
 f = open('docker-config.yaml')
 config = yaml.load(f)
 f.close()
-print config['master']
+print (config['master'])
 
 for worker in config['workers']
-    print worker['name']
-    print worker['ip']
+    print (worker['name'])
+    print (worker['ip'])
 
 instal()
-docker_instal(master)
+docker_instal(config[master])
 swarm_instal(args.hostnm, args.hosttyp)
 
 if __name__ == "__main__":
